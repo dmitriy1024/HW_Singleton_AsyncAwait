155a8e3e07d03cf96baca5d5f1f2483226fd3001
dmitriy1024 <dfedoryshyn@gmail.com>
2015-09-13 16:51:39 +0200

    HW_Singleton_AsyncAwait

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var stringsFromDB = await Task.Run(() => GetFromDB());
+            
+            return View(stringsFromDB);
+        }
+
+        public ActionResult TestLogging()
+        {
+            Logger.Instance.Write("some message");
+            return View("Error");
+        }
+
+        private List<string> GetFromDB()
+        {
+            var list = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add("Lorem ipsum dolor sit amet");
+            }
+
+            return list;
+        }
+    }   
+}
---
+++ WebApp/Logger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace WebApp
+{
+    public enum MessageType
+    {
+        Warn,
+        Error,
+        Info
+    }
+
+    public class Logger
+    {
+        private static readonly Lazy<Logger> _instance = new Lazy<Logger>(() => new Logger(), true);
+        private static readonly object _locker = new object();
+
+        private Logger() { }
+
+        public static Logger Instance
+        {
+            get { return _instance.Value; }
+        }
+
+        public void Write(Exception e, MessageType messType = MessageType.Info)
+        {
+            WriteToFile(String.Format("{0} - {1} - {2} - {3}", messType, DateTime.Now, e.GetType(), e.Message));
+        }
+
+        public void Write(Exception e, string message, MessageType messType = MessageType.Info)
+        {
+            WriteToFile(String.Format("{0} - {1} - {2} - {3}", messType, DateTime.Now, e.GetType(), message));
+        }
+
+        public void Write(string message, MessageType messType = MessageType.Info)
+        {
+            WriteToFile(String.Format("{0} - {1} - {2}", messType, DateTime.Now, message));
+        }
+
+        private void WriteToFile(string str)
+        {
+            StreamWriter writer = new StreamWriter(@"D:\log.txt",true);
+            writer.WriteLine(str);
+            writer.Close();
+        }
+    }
+}
